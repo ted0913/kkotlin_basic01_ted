3b0f6a814f9865f74e7c476858a02cb157dda48a
TED <newlife913@naver.com>
2021-11-05 01:05:15 +0900

    log push

--- app/src/main/java/com/kgschem/kkotlinbasic01_ted/MainActivity.kt
+++ app/src/main/java/com/kgschem/kkotlinbasic01_ted/MainActivity.kt
@@ -2,10 +2,23 @@ package com.kgschem.kkotlinbasic01_ted
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn.setOnClickListener {
+
+            // memo
+            Ctrl + / :
+
+            Log.d("main log", "click btn push")
+
+
+        }
+
     }
 }
